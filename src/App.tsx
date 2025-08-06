193acd729ce878068745475a7e1b70fb2566e8ed
narayanabhogav <narayana.bhogavarapu@sasi.ac.in>
2025-08-06 11:48:04 +0530

    Revert "chnages"
    This reverts commit 85aaab5a6a7da08650c344b49f3fed0436d385b5.

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,6 @@ import ResilienceScoreAnalyzer from './pages/assessment/ResilienceScoreAnalyzer'
 import BurnoutRiskAssessment from './pages/assessment/BurnoutRiskAssessment';
 import ProductivityStyleQuiz from './pages/assessment/ProductivityStyleQuiz';
 import LeadershipAssessment from './pages/assessment/LeadershipAssessment';
-import MoneyFlowAssessment from './pages/assessment/moneyflow/App';
 import AdminInsights from './pages/admin/Insights';
 import PrivacyPolicy from "./pages/PrivacyPolicy";
 import TermsAndConditions from "./pages/TermsAndConditions";
@@ -52,7 +51,6 @@ const App = () => (
               <Route path="/tools/assessments" element={<Assessments />} />
               <Route path="/tools/affirmations" element={<Affirmations />} />
               <Route path="/srcs" element={<Reflect />} />
-              <Route path="/moneyflow" element={<MoneyFlowAssessment />} />
               <Route path="/assessment/mental-fitness-index" element={<MentalFitnessIndexPage />} />
               <Route path="/assessment/entrepreneurial-potential" element={<EntrepreneurialPotential />} />
               <Route path="/assessment/emotionalintelligenceevaluator" element={<EmotionalIntelligenceEvaluator />} />
